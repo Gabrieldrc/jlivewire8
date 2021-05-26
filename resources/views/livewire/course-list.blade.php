67a261873a32379e5a96aa99d63ccf0191cf468c
Gabriel <you@example.com>
2021-05-26 19:44:08 -0300

    Se realiza los cambios al componente para que muestre lo que deseamos

--- resources/views/livewire/course-list.blade.php
+++ resources/views/livewire/course-list.blade.php
@@ -1,7 +1,20 @@
-<ul>
+<div class="grid grid-cols-3 gap-4 mt-8">
     @foreach ($courses as $course)
-    <li>
-        {{ $course->id}}
-    </li>
+    <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
+        <a href="">
+            <img
+                src="{{ $course->image }}"
+                alt="Imagen del blog"
+                class="rounded-md mb-2"
+                >
+                <h2 class="text-lg text-gray-600 truncate text-uppercase">{{ $course->name }}</h2>
+                <h3 class="text-md text-gray-500">{{ $course->excerpt }}</h3>
+                <img
+                    src="{{ $course->user->avatar }}"
+                    alt=""
+                    class="rounded-full mt-4 mx-auto h-16 w-16"
+                    >
+        </a>
+    </div>
     @endforeach
-</ul>
+</div>
